86e75fd40b4431f2a5bab479ad921c9aa7ab59b0
Brahim Amssayef <142234204+bamssaye@users.noreply.github.com>
2024-02-03 04:50:26 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,52 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bamssaye <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 04:49:58 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/02/02 17:45:44 by bamssaye         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/01/18 04:49:28 by bamssaye          #+#    #+#              #
+#    Updated: 2024/02/03 00:46:18 by bamssaye         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
-#include "headerfile/minitalk.h"
+NAME = client server
+NAME_BONUS  = client_bonus server_bonus
 
-void	ft_sendbits(int pid, int c);
+UTILS_M = utils/minitalk_utils.c
+UTILS_B = utils/minitalk_bonus_utils.c
+CLIENT_M = client.c
+SERVER_M = server.c
+CLIENT_B = client_bonus.c
+SERVER_B = server_bonus.c
 
-int	main(int ac, char **av)
-{
-	int	pid;
-	int	i;
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
 
-	i = 0;
-	pid = ft_atoi(av[1]);
-	if (ac == 3)
-	{
-		if ((kill(pid, 0) != 0) || (pid == 0))
-			return (ft_putstr(PIDNOTV), 0);
-		while (av[2][i])
-			ft_sendbits(pid, av[2][i++]);
-		ft_sendbits(pid, '\n');
-	}
-	else
-		ft_putstr(PIDERROR);
-	return (0);
-}
 
-void	ft_sendbits(int pid, int c)
-{
-	int	bit;
+all : $(NAME)
 
-	bit = 0;
-	while (bit <= 7)
-	{
-		if ((c >> bit & 1) != 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(150);
-		bit++;
-	}
-	usleep(100);
-}
+$(NAME): $(UTILS_M) $(CLIENT_M) $(SERVER_C)
+	@$(CC) $(CFLAGS) $(CLIENT_M) $(UTILS_M) -o client
+	@$(CC) $(CFLAGS) $(SERVER_M) $(UTILS_M) -o server
+	@echo "Mandatory Part : Compilation done."
+	
+bonus:$(NAME_BONUS)
+	
+$(NAME_BONUS): $(UTILS_B) $(CLIENT_B) $(SERVER_B)
+		@$(CC) $(CFLAGS) $(CLIENT_B) $(UTILS_B) -o client_bonus
+		@$(CC) $(CFLAGS) $(SERVER_B) $(UTILS_B) -o server_bonus
+		@echo "Bonus part : Compilation done."
+
+clean :	
+	@$(RM) client server client_bonus server_bonus
+fclean : clean
+	
+re : fclean all
+
+.PHONY : all, clean, fclean, re, bonus
